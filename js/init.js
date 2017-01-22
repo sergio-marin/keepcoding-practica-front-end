a606035c8841ac220acce44a3df6fa1e2f142ef2
Sergio Marin <serjey@gmail.com>
2017-01-22 22:29:00 +0100

    Añado scroll spy

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@
  jQuery(document).ready(function($) {
 
 /*----------------------------------------------------*/
-/* Smooth Scrolling
+/* Smooth Scroll
 ------------------------------------------------------ */
 
    $('.smoothscroll').on('click',function (e) {
@@ -22,3 +22,31 @@
 	        window.location.hash = target;
 	    });
 	});
+
+/*----------------------------------------------------*/
+/* Highlight current section in navigation bar
+------------------------------------------------------*/
+
+	$(window).on("scroll", function() {
+
+		var currentPos = $(window).scrollTop();
+
+		$('.navbar li a').each(function() {
+
+			var sectionLink = $(this);
+    		// capture the height of the navbar
+    		var navHeight = $('#nav-wrap').outerHeight() + 1;
+    		var section = $(sectionLink.attr('href'));
+
+    		// subtract the navbar height from the top of the section
+    		if(section.position().top - navHeight  <= currentPos && sectionLink.offset().top + section.height()> currentPos) {
+      			$('.navbar li').removeClass('active');
+      			sectionLink.parent().addClass('active');
+    		} else {
+    			sectionLink.parent().removeClass('active');
+    		}
+  		});        
+	});
+
+
+});
