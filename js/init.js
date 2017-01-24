c7dbc5e3c4abed227a965eb7ab4b559d38756784
Sergio Marin <serjey@gmail.com>
2017-01-24 01:00:00 +0100

    Finalizadas secciones Estudios y Experiencia y añadido contenido dummy para el resto

--- js/init.js
+++ js/init.js
@@ -48,5 +48,26 @@
   		});        
 	});
 
+/*----------------------------------------------------*/
+/*	Fade In/Out Primary Navigation
+------------------------------------------------------*/
+
+	$(window).on('scroll', function() {
+
+   		var h = $('header').height();
+		var y = $(window).scrollTop();
+      	var nav = $('#nav-wrap');
+
+      	if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+			nav.fadeOut('fast');
+		} else {
+			if (y < h*.20) {
+            nav.removeClass('opacidad').fadeIn('fast');
+        	} else {
+            	nav.addClass('opacidad').fadeIn('fast');
+         	}
+      	}
+    });
+
 
 });
