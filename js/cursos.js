dfad5eb092267539dd7180ec1ee791310fc04bdb
Sergio Marin <serjey@gmail.com>
2017-01-29 15:07:32 +0100

    Implementada la edición de cursos

--- js/cursos.js
+++ js/cursos.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	var fechaInput = $('#courseDate');
 	var descInput = $('#courseDescription');
 	var coursesContainer = $('#coursesContainer');
-	var loader = $('#image-loader');
+	var loader = $('#image-loader-cursos');
 	
 	var mostrarCursos = function () {
 		coursesContainer.empty();
@@ -18,7 +18,7 @@ $(document).ready(function() {
 			var contentToAdd = '';
 
 			for (var i = 0; i < cursos.length; i++) {
-				contentToAdd += '<div class="row course-container"><div class="curso-info col-11"><h3 class="tituloCurso">' + cursos[i].academia + '</h3><p class="info">' + cursos[i].titulo + '<span>  &bull;  </span><em class="fecha">' + cursos[i].fecha + '</em></p><p class="desc">' + cursos[i].desc + '</p></div><div class="curso-buttons col-1"><button class="editar" data-curso-id="' + cursos[i].id + '"><i class="fa fa-pencil"></i></button><button class="eliminar" data-curso-id="' + cursos[i].id + '"><i class="fa fa-trash-o"></i></button></div></div>';
+				contentToAdd += '<div class="row course-container"><div class="curso-info col-11"><h3 id="academia" class="academia curso' + cursos[i].id + '">' + cursos[i].academia + '</h3><p><span id="titulo" class="info curso' + cursos[i].id + '">' + cursos[i].titulo + '</span><span>  &bull;  </span><em id="fecha" class="fecha curso' + cursos[i].id + '">' + cursos[i].fecha + '</em></p><p id="desc" class="desc curso' + cursos[i].id + '">' + cursos[i].desc + '</p></div><div class="curso-buttons col-1"><button class="editar curso' + cursos[i].id + '" data-curso-id="' + cursos[i].id + '"><i class="fa fa-pencil"></i></button><button class="guardar  curso' + cursos[i].id + '" data-curso-id="' + cursos[i].id + '"><i class="fa fa-floppy-o"></i></button><button class="eliminar" data-curso-id="' + cursos[i].id + '"><i class="fa fa-trash-o"></i></button></div></div>';
 			}
 
 			coursesContainer.append(contentToAdd);
@@ -107,6 +107,36 @@ $(document).ready(function() {
 		});
 	}
 
+	var editarCurso = function(id, titulo, academia, fecha, desc) {
+		var data = {
+			'titulo': titulo,
+			'academia': academia,
+			'fecha': fecha,
+			'desc': desc
+		};
+
+		$.ajax({
+			type: "PUT",
+			url: API_URL + "cursos/" + id,
+			data: data
+		})
+		.done(function(data){
+			for (var i = 0; i < cursos.length; i++){
+				if(cursos[i].id == id) {
+					cursos[i].titulo = titulo;
+					cursos[i].academia = academia;
+					cursos[i].fecha = fecha;
+					cursos[i].desc = desc;
+				}
+			}
+
+			mostrarCursos();
+		})
+		.fail(function(error) {
+			console.error("Error al actualizar la información del curso.", error);
+		}) 
+	}
+
 	$('#enviarNuevoCurso').on("click", function(event){
 		if (tituloInput.val() != '' && academiaInput.val() != '' && fechaInput.val() != '') {
 			event.preventDefault();
@@ -119,7 +149,27 @@ $(document).ready(function() {
 		eliminarCurso(id);
 	});
 
+	$(document).on("click", ".editar", function(event){
+		var curso = ".curso" + $(this).data('cursoId');
+		$(".editar" + curso).hide();
+		$(".guardar" + curso).show();
+		$(this).parent().prev().find(curso).attr('contenteditable','true');
+		$(".academia" + curso).focus();
+	});
 
+	$(document).on("click", ".guardar", function(event){
+		var id = $(this).data('cursoId');
+		var curso = ".curso" + id;
+		var titulo = $(this).parent().prev().find('#titulo').text();
+		var academia = $(this).parent().prev().find('#academia').text();
+		var fecha = $(this).parent().prev().find('#fecha').text();
+		var desc = $(this).parent().prev().find('#desc').text();
+		console.log(titulo);
+		$(".guardar" + curso).hide();
+		$(".editar" + curso).show();
+		$(this).parent().prev().find(curso).attr('contenteditable','false');
+		editarCurso(id, titulo, academia, fecha, desc);
+	});
 
 	setTimeout(function() {
 		obtenerCursos();
